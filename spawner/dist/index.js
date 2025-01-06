7844cbfc3c060d20de9bf521601d34e2421485a9
Ketan Kauntia <ketankauntia26@gmail.com>
2025-01-06 12:52:18 +0530

    fixed everything : joins meeting done

--- spawner/dist/index.js
+++ spawner/dist/index.js
@@ -15,27 +15,29 @@ function main() {
     return __awaiter(this, void 0, void 0, function* () {
         //passing flags to selenium grid
         const chromeOptions = new chrome_1.Options();
-        chromeOptions.addArguments("--use-fake-ui-for-media-stream"); //to enable mic and camera :)
-        chromeOptions.addArguments("--disable-blink-features=AutomationControlled");
+        chromeOptions.addArguments('--use-fake-ui-for-media-stream'); //to enable mic and camera :)
+        chromeOptions.addArguments('--disable-blink-features=AutomationControlled');
         //WebDriver instance with Chrome
         let driver = yield new selenium_webdriver_1.Builder()
             .forBrowser(selenium_webdriver_1.Browser.CHROME)
             .setChromeOptions(chromeOptions) //while building the chrome instance we need to set automation disabled.
             .build();
         try {
-            yield driver.get("https://meet.google.com/nvm-osfi-gfa");
+            yield driver.get('https://meet.google.com/nvm-osfi-gfa');
             // waiting for the elements of the page to load
             yield driver.sleep(3000);
             const popupButton = yield driver.wait(selenium_webdriver_1.until.elementLocated(selenium_webdriver_1.By.xpath("//span[contains(text(),'Got it')]")));
             popupButton.click();
-            const nameInput = yield driver.wait(selenium_webdriver_1.until.elementLocated(selenium_webdriver_1.By.id("c11")), 10000);
+            // const nameInput = await driver.wait(until.elementLocated(By.id("c11")), 10000);
+            const nameInput = yield driver.wait(selenium_webdriver_1.until.elementLocated(selenium_webdriver_1.By.xpath('//input[@placeholder="Your name"]')), 10000);
             yield nameInput.clear();
             yield nameInput.click();
-            yield nameInput.sendKeys("value", "Meeting Rec Bot");
-            // await driver.wait(until.elementLocated(By.id("c12314")), 10000);
-            const buttonElement = yield driver.wait(selenium_webdriver_1.until.elementLocated(selenium_webdriver_1.By.xpath("//span[contains(text(),'Ask to join') or contains(text(),'Join')]")));
+            yield nameInput.sendKeys('value', 'Meeting Rec Bot');
+            yield driver.sleep(2000);
+            // await driver.wait(until.elementLocated(By.id('c12314')), 10000);
+            const buttonElement = yield driver.wait(selenium_webdriver_1.until.elementLocated(selenium_webdriver_1.By.xpath("//span[contains(text(),'Ask to join')]")));
             yield buttonElement.click();
-            yield driver.wait(selenium_webdriver_1.until.elementLocated(selenium_webdriver_1.By.id("c12314")), 10000);
+            yield driver.wait(selenium_webdriver_1.until.elementLocated(selenium_webdriver_1.By.id('c12314')), 10000);
         }
         finally {
             yield driver.quit();
--- spawner/src/index.ts
+++ spawner/src/index.ts
@@ -1,11 +1,11 @@
-import { Builder, Browser, By, Key, until } from "selenium-webdriver";
-import { Options } from "selenium-webdriver/chrome";
+import { Builder, Browser, By, Key, until } from 'selenium-webdriver';
+import { Options } from 'selenium-webdriver/chrome';
 
 async function main() {
   //passing flags to selenium grid
   const chromeOptions = new Options();
-  chromeOptions.addArguments("--use-fake-ui-for-media-stream"); //to enable mic and camera :)
-  chromeOptions.addArguments("--disable-blink-features=AutomationControlled");
+  chromeOptions.addArguments('--use-fake-ui-for-media-stream'); //to enable mic and camera :)
+  chromeOptions.addArguments('--disable-blink-features=AutomationControlled');
 
   //WebDriver instance with Chrome
   let driver = await new Builder()
@@ -14,7 +14,7 @@ async function main() {
     .build();
 
   try {
-    await driver.get("https://meet.google.com/nvm-osfi-gfa");
+    await driver.get('https://meet.google.com/nvm-osfi-gfa');
 
     // waiting for the elements of the page to load
     await driver.sleep(3000);
@@ -24,20 +24,23 @@ async function main() {
     );
     popupButton.click();
 
-    const nameInput = await driver.wait(until.elementLocated(By.id("c11")), 10000);
+    // const nameInput = await driver.wait(until.elementLocated(By.id("c11")), 10000);
+    const nameInput = await driver.wait(
+      until.elementLocated(By.xpath('//input[@placeholder="Your name"]')),
+      10000
+    );
     await nameInput.clear();
     await nameInput.click();
-    await nameInput.sendKeys("value", "Meeting Rec Bot");
-    // await driver.wait(until.elementLocated(By.id("c12314")), 10000);
+    await nameInput.sendKeys('value', 'Meeting Rec Bot');
+    await driver.sleep(2000);
+    // await driver.wait(until.elementLocated(By.id('c12314')), 10000);
 
     const buttonElement = await driver.wait(
-      until.elementLocated(
-        By.xpath("//span[contains(text(),'Ask to join') or contains(text(),'Join')]")
-      )
+      until.elementLocated(By.xpath("//span[contains(text(),'Ask to join')]"))
     );
 
     await buttonElement.click();
-    await driver.wait(until.elementLocated(By.id("c12314")), 10000);
+    // await driver.wait(until.elementLocated(By.id('c12314')), 10000);
   } finally {
     await driver.quit();
   }
